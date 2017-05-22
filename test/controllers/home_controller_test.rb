493e918aa5aa407dfd0da4ee472fddb616e092b5
Katarzyna Krzemińska <kat.krzeminska.kat@gmail.com>
2017-05-22 16:40:04 +0000

    Start a fire!

---
+++ test/controllers/home_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class HomeControllerTest < ActionController::TestCase
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
+
+  test "should get account" do
+    get :account
+    assert_response :success
+  end
+
+end
